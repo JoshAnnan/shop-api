6653f113e19da2998d74c5647b7b060bae2c9a01
JoshAnnan <annanjosh04@gmail.com>
2022-04-25 15:07:33 -0700

    authorization

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -10,11 +10,13 @@ const createProduct = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find({});
-    } catch (error) {
-       res.status(400).json({ error: error.message }); 
-    }
+  try {
+    console.log(req.user);
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const getSingleProduct = async (req, res) => {
